f85a3f2197bc848851e8a0e2b71748dd35a698dd
gingo21 <francois.reymond10@gmail.com>
2013-06-08 18:50:35 +0300

    derp

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -54,6 +54,7 @@ ifeq (pa_l900,$(TARGET_PRODUCT))
 endif
 ifeq (pa_find5,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_find5.mk
+endif
 ifeq (pa_crespo4g,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/pa_crespo4g.mk
 endif
